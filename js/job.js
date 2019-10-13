de7bddff618939871be26981eb07ddd2bcf5ae04
az <az@localhost>
2019-10-13 22:46:46 +0200

    jobs template refactor

--- js/job.js
+++ js/job.js
@@ -7,9 +7,11 @@ String.prototype.interpolate = function (params) {
 document.addEventListener("DOMContentLoaded", () => {
     fetch("job_template.html").then(response => response.text())
         .then(html => fetch("jobs.json").then(response => response.json()).then(json => {
-            const div = document.createElement("div");
-            div.innerHTML = html.interpolate(json);
-            document.getElementById("vacature_single_blok_inner").appendChild(div);
+            json.jobs.forEach(job => {
+                const div = document.createElement("div");
+                div.innerHTML = html.interpolate(job);
+                document.getElementById("vacature_single_blok_inner").appendChild(div);
+            });
             $(".vacature_btn_terug").click(function () {
                 $("#vacature_single_blok").animate({"height": "0px"}, 500, function () {
                     jQuery("html,body").animate({scrollTop: 0}, 500);
