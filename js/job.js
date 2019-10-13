204231a45aac8faa2ab0b644c6a20b2b5442d89b
az <az@localhost>
2019-10-13 22:58:47 +0200

    jobs template refactor

--- js/job.js
+++ js/job.js
@@ -4,19 +4,20 @@ String.prototype.interpolate = function (params) {
     return new Function(...names, `return \`${this}\`;`)(...values);
 };
 
-document.addEventListener("DOMContentLoaded", () => {
-    fetch("job_template.html").then(response => response.text())
-        .then(html => fetch("jobs.json").then(response => response.json()).then(json => {
-            json.jobs.forEach(job => {
-                const div = document.createElement("div");
-                div.innerHTML = html.interpolate(job);
-                document.getElementById("vacature_single_blok_inner").appendChild(div);
-            });
-            $(".vacature_btn_terug").click(function () {
-                $("#vacature_single_blok").animate({"height": "0px"}, 500, function () {
-                    jQuery("html,body").animate({scrollTop: 0}, 500);
-                    $(".vacature_content_blok").css("display", "none");
-                })
-            });
-        }));
-});
+async function jobs() {
+    const jobTemplate = await fetch("job_template.html").then(response => response.text());
+    const jobData = await fetch("jobs.json").then(response => response.json());
+    jobData.jobs.forEach(job => {
+        const div = document.createElement("div");
+        div.innerHTML = jobTemplate.interpolate(job);
+        document.getElementById("vacature_single_blok_inner").appendChild(div);
+    });
+    $(".vacature_btn_terug").click(function () {
+        $("#vacature_single_blok").animate({"height": "0px"}, 500, function () {
+            jQuery("html,body").animate({scrollTop: 0}, 500);
+            $(".vacature_content_blok").css("display", "none");
+        })
+    });
+}
+
+document.addEventListener("DOMContentLoaded", jobs);
