e687a2e3f6dd3468d528d312cc4d00b5022dacc8
az <az@localhost>
2019-10-13 23:23:39 +0200

    jobs template refactor

--- js/job.js
+++ js/job.js
@@ -5,19 +5,34 @@ String.prototype.interpolate = function (params) {
 };
 
 async function jobs() {
-    const jobTemplate = await fetch("job_template.html").then(response => response.text());
+    const jobTemplate = await fetch("template/job_template.html").then(response => response.text());
+    const jobTileTemplate = await fetch("template/job_tile_template.html").then(response => response.text());
     const jobData = await fetch("jobs.json").then(response => response.json());
+    let jobHtml = "";
+    let jobTileHtml = "";
     jobData.jobs.forEach(job => {
-        const div = document.createElement("div");
-        div.innerHTML = jobTemplate.interpolate(job);
-        document.getElementById("vacature_single_blok_inner").appendChild(div);
+        jobHtml += jobTemplate.interpolate(job);
+        jobTileHtml += jobTileTemplate.interpolate(job);
     });
+    document.getElementById("vacature_single_blok_inner").innerHTML = jobHtml + "<div style=\"clear:both;height:0;\"></div>";
+    document.getElementById("vacature_verzamel_blok_inner").innerHTML = jobTileHtml + "<div style=\"clear:both;height:0;\"></div>";
     $(".vacature_btn_terug").click(function () {
         $("#vacature_single_blok").animate({"height": "0px"}, 500, function () {
             jQuery("html,body").animate({scrollTop: 0}, 500);
             $(".vacature_content_blok").css("display", "none");
         })
     });
+    $(".vacature_item").click(function () {
+        const ind = $(this).index();
+        $("#vacature_single_blok").animate({"height": "0px", "marginBottom": "0px"}, 500, () => {
+            jQuery("html,body").animate({scrollTop: 0}, 500);
+            $(".vacature_content_blok").css("display", "none");
+            $(".vacature_content_blok:eq(" + ind + ")").css("display", "block");
+            const height = $(".vacature_content_blok:eq(" + ind + ")").height() + 80;
+            $("#vacature_single_blok").animate({"height": height + "px", "marginBottom": "30px"}, 500);
+        });
+    });
+    $(".vacature_item_hover").each(() => $(this).height($(this).parent().parent().height()));
 }
 
 document.addEventListener("DOMContentLoaded", jobs);
